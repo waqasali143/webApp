475072b5debf883b514dbcd41c59d5f713507668
Waqas Ali <82609521+waqasali143@users.noreply.github.com>
2026-02-08 18:33:16 +0500

    Update newApp.py

--- newApp.py
+++ newApp.py
@@ -34,7 +34,7 @@ if uploaded_file is not None:
         # Display the report in Streamlit
         with open("report.html", "r", encoding="utf-8") as f:
             html_content = f.read()
-        components.html(html_content, height=800, scrolling=True)
+        components.html(html_content, height=600, scrolling=True)
 
 else:
     st.info("Please upload a CSV file to get started.")
